974c958b2d3270495f149556625982e81a18adc3
Michael Brown <gamer5108@hotmail.com>
2015-09-28 21:28:08 -0600

    Add fire delay to player

--- source/Player.js
+++ source/Player.js
@@ -11,6 +11,10 @@ function Player(health, x, y)
     this.BARREL_OFFSET_TO_BASE = 25;
     this.BARREL_OFFSET_TO_TIP = 70;
     this.BULLET_SPEED = 300;
+    this.BULLET_FIRE_DELAY = .5;
+
+    this.bulletFireDelayCur = 0;
+    this.canFire = true;
 
     this.PLAYER_ROTATE_SPEED = Math.PI;
 
@@ -29,12 +33,24 @@ Player.prototype.constructor = Player;
 Player.prototype.Update = function(gameTime)
 {
     GameObject.prototype.Update.call(this, gameTime);
-	if(Player.controls.space)
+
+    if(!this.canFire)
+    {
+        this.bulletFireDelayCur += gameTime;
+        if(this.bulletFireDelayCur >= this.BULLET_FIRE_DELAY)
+        {
+            this.bulletFireDelayCur = 0;
+            this.canFire = true;
+        }
+    }
+
+	if(Player.controls.space && this.canFire)
 	{
         var facing = this.GetFacing();
         var startPosition = VectorAdd({x:this.barrel.x,y:this.barrel.y}, VectorMultiply(this.BARREL_OFFSET_TO_BASE+this.BARREL_OFFSET_TO_TIP, facing));
         var velocity = VectorMultiply(this.BULLET_SPEED,facing);
         var bullet = new Bullet("tempshot.png", velocity, startPosition.x, startPosition.y);
+        this.canFire = false;
 	}
 
   	if(Player.controls.left)
