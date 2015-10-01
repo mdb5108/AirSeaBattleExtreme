61dc23788ccc4dc3a12af567b0f1173249f13355
Michael Brown <gamer5108@hotmail.com>
2015-10-01 09:46:00 -0600

    Add offset to text banner

--- source/GameExitState.js
+++ source/GameExitState.js
@@ -30,12 +30,12 @@ GameExitState.prototype.Enter = function()
     var winText = "PLAYER 1 WINS!!!";
 
     GameManager.Pause();
-    this.text = new TextBanner(winText, 50);
+    this.text = new TextBanner(winText, 50, 0);
     var gameExitState = this;
     this.resetTimeout = setTimeout(function(){
         gameExitState.resetProxy = gameExitState.ResetProxy();
         gameExitState.text.Destroy();
-        gameExitState.text = new TextBanner(winText+"\r\nPress FIRE to restart", 50);
+        gameExitState.text = new TextBanner(winText+"\r\nPress FIRE to restart", 50, 0);
     }, RESTART_DELAY);
 };
 
--- source/TextBanner.js
+++ source/TextBanner.js
@@ -1,8 +1,9 @@
-function TextBanner(text, fontSize)
+function TextBanner(text, fontSize, yoffset)
 {
     GameObject.call(this, "", 0, 0, 0, 0);
     this.text = text;
     this.fontSize = fontSize;
+    this.yoffset = yoffset;
 };
 
 TextBanner.prototype = Object.create(GameObject.prototype);
@@ -13,6 +14,6 @@ TextBanner.prototype.Draw = function(canvas2D)
     canvas2D.save();
     canvas2D.font = this.fontSize+"px Ariel";
     canvas2D.textAlign = "center";
-    canvas2D.fillText(this.text, GameManager.CANVAS_WIDTH/2, GameManager.CANVAS_HEIGHT/2);
+    canvas2D.fillText(this.text, GameManager.CANVAS_WIDTH/2, GameManager.CANVAS_HEIGHT/2 + this.yoffset);
     canvas2D.restore();
 };
