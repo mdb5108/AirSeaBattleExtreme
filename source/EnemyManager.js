098f0a3be5a1182d15fa55911fc6f44949e7b6c9
Michael Brown <gamer5108@hotmail.com>
2015-10-06 10:22:10 -0600

    Change Ufo to 1 out of 100

--- source/EnemyManager.js
+++ source/EnemyManager.js
@@ -18,7 +18,7 @@ function EnemyManager()
     this.max_lanes = 7;
     this.planes_path = ["airplane1.png","plane_2.png","plane_3.png","plane_4.png","plane_5.png","plane_6.png", "golden.png"];
     this.plane_type_min = 1;
-    this.plane_type_max = 23;
+    this.plane_type_max = 100;
     this.plane_type = 0;
     // this.sound_paths = ["Plane_01.mp3","Plane_01.mp3","Fast Jet_01.mp3","Fast Jet_01.mp3","Helicopter_01.mp3","Helicopter_01.mp3"];
     this.sound_paths = ["no_sound","no_sound","Fast Jet_01.mp3","Fast Jet_01.mp3","Helicopter_01.mp3","Helicopter_01.mp3", "no_sound"];
@@ -38,6 +38,14 @@ EnemyManager.prototype.Update = function(gametime) {
         {
             case 1:
             case 2:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
                 this.plane_type = 2;
                 veloObj = new Vec2(400,0);
                 points = 10;
@@ -45,6 +53,14 @@ EnemyManager.prototype.Update = function(gametime) {
                 break;
             case 3:
             case 4:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
                 this.plane_type = 3;
                 veloObj = new Vec2(400,0);
                 points = 10;
@@ -53,6 +69,18 @@ EnemyManager.prototype.Update = function(gametime) {
             case 5:
             case 6:
             case 7:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
                 this.plane_type = 5;
                 veloObj = new Vec2(200,0);
                 points = 5;
@@ -61,6 +89,18 @@ EnemyManager.prototype.Update = function(gametime) {
             case 8:
             case 9:
             case 10:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+            case 60:
                 this.plane_type = 4;
                 veloObj = new Vec2(200,0);
                 points = 5;
@@ -71,6 +111,26 @@ EnemyManager.prototype.Update = function(gametime) {
             case 13:
             case 14:
             case 15:
+            case 61:
+            case 62:
+            case 63:
+            case 64:
+            case 65:
+            case 66:
+            case 67:
+            case 68:
+            case 69:
+            case 70:
+            case 71:
+            case 72:
+            case 73:
+            case 74:
+            case 75:
+            case 76:
+            case 77:
+            case 78:
+            case 79:
+            case 80:
                 this.plane_type = 0;
                 veloObj = new Vec2(100,0);
                 points = 1;
@@ -81,14 +141,31 @@ EnemyManager.prototype.Update = function(gametime) {
             case 18:
             case 19:
             case 20:
+            case 81:
+            case 82:
+            case 83:
+            case 84:
+            case 85:
+            case 86:
+            case 87:
+            case 88:
+            case 89:
+            case 90:
+            case 91:
+            case 92:
+            case 93:
+            case 94:
+            case 95:
+            case 96:
+            case 97:
+            case 98:
+            case 99:
                 this.plane_type = 1;
                 veloObj = new Vec2(100,0);
                 points = 1;
                 health = 1;
                 break;
-            case 21:
-            case 22:
-            case 23:
+            case 100:
                 this.plane_type = 7;
                 veloObj = new Vec2(100, 0);
                 points = 20;
