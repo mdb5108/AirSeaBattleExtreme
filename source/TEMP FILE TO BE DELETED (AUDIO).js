4277dfde9bc8d3e012ef25b234c8174cf8c8f888
mdb5108 <gamer5108@hotmail.com>
2015-10-19 12:05:53 -0600

    Remove files which have changed location to folders in Art's changes

--- source/TEMP FILE TO BE DELETED (AUDIO).js
+++
@@ -1,9 +0,0 @@
-
-function BackMusic()
-{
-    // this.musicPath = introMusic; 
-    // this.introMusic = new Audio(this.musicPath);
-    // this.introMusic.loop = true;
-    // this.introMusic.play();    
-};
-
